aad158d2c86702ea21014bfbfe6d7b065a77f2b9
JianKowalski <jean_kowalski@hotmail.com>
2017-09-25 22:23:47 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/kowalski/api/ProjetoStsApplication.java
@@ -0,0 +1,12 @@
+package com.kowalski.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoStsApplication.class, args);
+	}
+}
